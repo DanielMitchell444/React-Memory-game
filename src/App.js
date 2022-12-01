755e71174d04d459020b4d965162d20760be53fa
Daniel Mitchell <mitchelldaniel426@gmail.com>
2022-12-01 14:57:15 -0800

    commit message

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import data from '../src/Components/data';
 import { useState } from 'react';
 
 function App() {
-  const [cardArray, card] = useState([]);
+  const [cardArray, card] = useState(data);
+  const [images, setImages] = useState(data);
 
-  const matchCard = (e) => {
-      console.log(e.target);
+  const matchCard = () => {
+      console.log(shuffleArray(data));
   }
 
   const shuffleArray = (array) => {
@@ -17,24 +18,35 @@ function App() {
    const temp = array[i];
    array[i] = array[j];
    array[j] = temp;
-   }
+   cardArray(card => [...card, array]);
 
+   console.log(cardArray);
+   }
    return array;
   }
 
 
+  const compareCards = (index) => {
+  console.log(cardArray);
+  }
+
+  let shuffle = shuffleArray(data);
+
   return (
     <div className= {styles.App}>
-    {data.map((datas) => {
+    {shuffle.map((datas) => {
      return <Card
      image = {datas.image}
      name = {datas.name}
      keys = {datas.id}
      click = {matchCard}
      shuffle = {shuffleArray}
+     compare = {compareCards}
 
      />
     })}
+
+
     </div>
   );
 
