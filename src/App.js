b511422dc13d1047cd1ee68035d39e2471bb3b4a
Daniel Mitchell <mitchelldaniel426@gmail.com>
2022-12-01 19:14:04 -0800

    committ

--- src/App.js
+++ src/App.js
@@ -3,34 +3,34 @@ import  styles from './App.module.css';
 import Card from './Components/Card';
 import data from '../src/Components/data';
 import { useState } from 'react';
+import {useEffect} from 'react';
 
 function App() {
-  const [cardArray, card] = useState(data);
+  const [cardArray, setCardArray] = useState(data);
   const [images, setImages] = useState(data);
 
   const matchCard = () => {
-      console.log(shuffleArray(data));
+     
   }
 
-  const shuffleArray = (array) => {
-   for(let i = 0; i > 0; i--){
-   const j = Math.floor(Math.random() * ( i + 1));
-   const temp = array[i];
-   array[i] = array[j];
-   array[j] = temp;
-   cardArray(card => [...card, array]);
+    function shuffle(array) {
+      let currentIndex = array.length,  randomIndex;
+    
+      // While there remain elements to shuffle.
+      while (currentIndex != 0) {
+    
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+    
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+    
+      return array;
+    }
 
-   console.log(cardArray);
-   }
-   return array;
-  }
-
-
-  const compareCards = (index) => {
-  console.log(cardArray);
-  }
-
-  let shuffle = shuffleArray(data);
 
   return (
     <div className= {styles.App}>
@@ -40,8 +40,7 @@ function App() {
      name = {datas.name}
      keys = {datas.id}
      click = {matchCard}
-     shuffle = {shuffleArray}
-     compare = {compareCards}
+     shuffle = {shuffle(data)}
 
      />
     })}
